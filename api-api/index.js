34af1247c2b78c7702213a50567d8a652174e9bb
Łukasze <lukaszkucharzyk17@gmail.com>
2023-10-21 12:17:35 +0200

    .

--- api-api/index.js
+++ api-api/index.js
@@ -3,10 +3,10 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const apiProxy = createProxyMiddleware({
   target: "https://api.steampowered.com",
   changeOrigin: true,
-  //   onProxyRes(proxyRes) {
-  //     proxyRes.headers["x-added"] = "foobar"; // add new header to response
-  //     delete proxyRes.headers["x-removed"]; // remove header from response
-  //   },
+  onProxyRes(proxyRes) {
+    proxyRes.headers["x-added"] = "foobar"; // add new header to response
+    delete proxyRes.headers["x-removed"]; // remove header from response
+  },
 });
 
 // Expose the proxy on the "/api/*" endpoint.
